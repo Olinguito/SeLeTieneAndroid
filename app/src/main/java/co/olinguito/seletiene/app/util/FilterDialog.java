c4721f246884761bdf64ece6e19db0c09607ced8
Daniel Olano <danielolanoc@gmail.com>
2015-02-03 18:39:21 -0500

    [list][filter] select department

--- app/src/main/java/co/olinguito/seletiene/app/util/FilterDialog.java
+++ app/src/main/java/co/olinguito/seletiene/app/util/FilterDialog.java
@@ -3,14 +3,12 @@ package co.olinguito.seletiene.app.util;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RatingBar;
+import android.widget.*;
 import co.olinguito.seletiene.app.R;
 
 import java.util.HashMap;
@@ -25,6 +23,7 @@ public class FilterDialog extends DialogFragment implements View.OnClickListener
     private RadioButton mServiceCheck;
     private RatingBar mRatingBar;
     private EditText mQuery;
+    private Spinner mDepartment;
 
     public interface FilterListener {
         public void onFilterChange(HashMap<String, String> searchParams);
@@ -54,6 +53,10 @@ public class FilterDialog extends DialogFragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         View view = inflater.inflate(R.layout.filter_view, container, false);
+        mDepartment = (Spinner) view.findViewById(R.id.deparment);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.deparments, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mDepartment.setAdapter(adapter);
         mAllCheck = (RadioButton) view.findViewById(R.id.filter_all);
         mServiceCheck = (RadioButton) view.findViewById(R.id.filter_service);
         mProductCheck = (RadioButton) view.findViewById(R.id.filter_product);
@@ -81,6 +84,13 @@ public class FilterDialog extends DialogFragment implements View.OnClickListener
             mAllCheck.setChecked(true);
         mRatingBar.setRating(Float.parseFloat(mParams.get("minStars")));
         mQuery.setText(mParams.get("q"));
+        //
+
+        int department;
+        if (mParams.containsKey("departmentId"))
+            department = Integer.parseInt(mParams.get("departmentId"));
+        else department = 0;
+        mDepartment.setSelection(department);
     }
 
     public void setFilterChangeListener(FilterListener listener) {
@@ -97,9 +107,26 @@ public class FilterDialog extends DialogFragment implements View.OnClickListener
         else
             mParams.remove("type");
         mParams.put("q", mQuery.getText().toString());
+        //
+        int departmentId = mDepartment.getSelectedItemPosition();
+        if (departmentId == 0)
+            mParams.remove("departmentId");
+        else
+            mParams.put("departmentId", String.valueOf(departmentId));
 
         if (mFilterListener != null)
             mFilterListener.onFilterChange(mParams);
         dismiss();
+    }
+
+    private int findDeparmentId(String department) {
+        Log.d("FILTER>>>", String.valueOf(department));
+        SpinnerAdapter adapter = mDepartment.getAdapter();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            String selection = (String) adapter.getItem(i);
+            Log.d("FILTER>", selection);
+            if (department.equals(selection)) return i;
+        }
+        return -1;
     }
 }
