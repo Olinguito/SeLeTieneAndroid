22e0f1aedfa0ab43a952a8a263ca10c970386675
Daniel Olano <danielolanoc@gmail.com>
2015-02-09 13:54:18 -0500

    fix: rounded images with wrong size Images where not respecting the crop rules this fixes it but doesn't support antialiasing

--- app/src/main/java/co/olinguito/seletiene/app/util/RoundedImageView.java
+++ app/src/main/java/co/olinguito/seletiene/app/util/RoundedImageView.java
@@ -2,19 +2,23 @@ package co.olinguito.seletiene.app.util;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.*;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import co.olinguito.seletiene.app.R;
 import com.android.volley.toolbox.NetworkImageView;
 
+/**
+ * RoundedImageView
+ * configure radius from XML
+ * // TODO: make borders antialiased
+ */
 public class RoundedImageView extends NetworkImageView {
 
     private final float defRadius = 3.0f;
-    private RectF mRectF;
     float mRadius;
-    private Paint mPaint;
+    private Path clipPath = new Path();
 
     public RoundedImageView(Context context) {
         this(context, null);
@@ -40,36 +44,10 @@ public class RoundedImageView extends NetworkImageView {
     }
 
     @Override
-    public void setImageBitmap(Bitmap bm) {
-        super.setImageBitmap(bm);
-        if (bm != null) {
-            mPaint = getPaint(bm);
-            mRectF = new RectF(0.0f, 0.0f, getWidth(), getHeight());
-        }
-    }
-
-    @Override
-    public void setImageResource(int resId) {
-        super.setImageResource(resId);
-        Drawable d = getDrawable();
-        Bitmap bitmap = ((BitmapDrawable) d.mutate()).getBitmap();
-        if (bitmap != null)
-            setImageBitmap(bitmap);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
-        if (mPaint == null)
-            return;
-        mRectF.set(0.0f, 0.0f, getWidth(), getHeight());
-        canvas.drawRoundRect(mRectF, getRadius(), getRadius(), mPaint);
-    }
-
-    private Paint getPaint(Bitmap bmp) {
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        BitmapShader shader = new BitmapShader(bmp, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        paint.setShader(shader);
-        return paint;
+        RectF rect = new RectF(0, 0, getWidth(), getHeight());
+        clipPath.addRoundRect(rect, getRadius(), getRadius(), Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
     }
 }
